671e2eb465a3238f9602ce766e75f9fd9ab0194f
luismarioamor <luismarioamorsanchez@gmail.com>
2024-04-14 20:46:19 -0500

    configuration pokemoncard

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
-
+const remotes = (isServer) => {
+  const location = isServer ? "ssr" : "chunks";
+  return {
+    hostShell: `host-shell@http://localhost:3000/_next/static/${location}/remoteEntry.js`,
+  };
+};
 const nextConfig = {
   reactStrictMode: true,
   webpack(config, options) {
@@ -8,19 +13,21 @@ const nextConfig = {
       new NextFederationPlugin({
         name: "pokemon-one",
         filename: "static/chunks/remoteEntry.js",
-        remotes: {
-          hostShell:
-            "host-shell@http://localhost:3000/_next/static/chunks/remoteEntry.js",
-        },
+        remotes: remotes(options.isServer),
         exposes: {
-          "./button": "./components/Button.tsx",
+          "./pokemonOne": "./components/PokemonOne/index.tsx",
         },
         extraOptions: {
           exposePages: true,
         },
       })
     );
+
     return config;
+  },
+
+  images: {
+    domains: ["raw.githubusercontent.com"],
   },
 };
 
--- pages/types.d.ts
+++ pages/types.d.ts
@@ -1,5 +1,20 @@
-declare module "pokemonOne/button" {
+declare module "hostShell/card" {
   import React from "react";
-  const Button: React.FC<{}>;
-  export default Button;
+  import { IPokemon, IPokemonSpecies } from "@/types";
+
+  interface IProps {
+    src: string;
+    bgColorCard: string;
+    pokemon: IPokemon;
+    pokemonSpecies: IPokemonSpecies;
+  }
+
+  const CardPokemon: React.FC<IProps>;
+  export default CardPokemon;
+}
+
+declare module "pokemonOne/pokemonOne" {
+  import React from "react";
+  const PokemonOne: React.FC<{}>;
+  export default PokemonOne;
 }
